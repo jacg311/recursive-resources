b3e8c8155f1fdcac85e614be42f03953267d95aa
enjarai <enjarai@protonmail.com>
2022-12-02 10:12:23 +0100

    Fix folder texture resource pack support (#10) and add maven publishing.

--- src/main/java/nl/enjarai/recursiveresources/packs/FolderPack.java
+++ src/main/java/nl/enjarai/recursiveresources/packs/FolderPack.java
@@ -8,6 +8,7 @@ import net.minecraft.resource.ResourcePackCompatibility;
 import net.minecraft.resource.ResourcePackSource;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
+import nl.enjarai.recursiveresources.RecursiveResources;
 import org.apache.logging.log4j.LogManager;
 import org.jetbrains.annotations.Nullable;
 
@@ -15,23 +16,8 @@ import java.io.File;
 import java.io.InputStream;
 
 public class FolderPack implements ResourcePackOrganizer.Pack {
-    private static final Identifier folderResource = new Identifier("recursiveresources:textures/gui/folder.png");
-    private static final Identifier openFolderResource = new Identifier("recursiveresources:textures/gui/folder_open.png");
-
-    static {
-        // for some reason the texture fails to load in the actual game
-        loadTexture(folderResource, "/assets/recursiveresources/textures/gui/folder.png");
-        loadTexture(openFolderResource, "/assets/recursiveresources/textures/gui/folder_open.png");
-    }
-
-    private static void loadTexture(Identifier id, String path) {
-        try (InputStream stream = FolderPack.class.getResourceAsStream(path)) {
-            MinecraftClient.getInstance().getTextureManager().registerTexture(id, new NativeImageBackedTexture(NativeImage.read(stream)));
-        } catch (Exception e) {
-            LogManager.getLogger(FolderPack.class).warn("Error loading folder texture:");
-            e.printStackTrace();
-        }
-    }
+    private static final Identifier FOLDER_TEXTURE = RecursiveResources.id("textures/gui/folder.png");
+    private static final Identifier OPEN_FOLDER_TEXTURE = RecursiveResources.id("textures/gui/folder_open.png");
 
     private static Identifier loadCustomIcon(File icon, File relativeFolder) {
         if (icon != null && icon.exists()) {
@@ -76,7 +62,7 @@ public class FolderPack implements ResourcePackOrganizer.Pack {
 
     @Override
     public Identifier getIconId() {
-        return icon != null ? icon : (hovered ? openFolderResource : folderResource);
+        return icon != null ? icon : (hovered ? OPEN_FOLDER_TEXTURE : FOLDER_TEXTURE);
     }
 
     @Override
